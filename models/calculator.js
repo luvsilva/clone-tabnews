f992280f8a4708be836bb78d4a285b31fbf2cbfe
Lucas Silva <41387497+luvsilva@users.noreply.github.com>
2025-01-31 03:33:17 +0000

    Automated test

---
+++ models/calculator.js
@@ -0,0 +1,8 @@
+function sum(arg1, arg2) {
+  if (typeof arg1 !== "number" || typeof arg2 !== "number") {
+    return "Erro";
+  }
+  return arg1 + arg2;
+}
+
+exports.sum = sum;
--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,16 @@
-test("expect that 1 be 1", () => {
-  expect(1).toBe(10);
+const calculator = require("../models/calculator.js");
+
+test("Sum 2 + 2 should return 4", () => {
+  const result = calculator.sum(2, 2);
+  expect(result).toBe(4);
+});
+
+test("Sum 5 + 100 should return 105", () => {
+  const result = calculator.sum(5, 100);
+  expect(result).toBe(105);
+});
+
+test("Sum 5 + banana should return 105", () => {
+  const result = calculator.sum(5, "banana");
+  expect(result).toBe("Erro");
 });
